6e3af08ff11566b6d68979e7c6ba3d0ec00554ee
faruckchavarria <faruckchavarria2@gmail.com>
2020-12-13 18:42:47 -0600

    se agrega diseño de formulario de registro de producto

--- app/src/main/java/com/cuandoeramilitar/proyecto_final/HomeActivity.kt
+++ app/src/main/java/com/cuandoeramilitar/proyecto_final/HomeActivity.kt
@@ -1,11 +1,17 @@
 package com.cuandoeramilitar.proyecto_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+    fun  LanzarRegistrar(view: View) {
+        val intent = Intent(this,Principal ::class.java)
+        startActivity(intent)
     }
 }
