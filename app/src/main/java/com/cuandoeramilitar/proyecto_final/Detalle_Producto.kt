9255111e86ed694c08cda5f994e8f5725b9e217c
faruckchavarria <faruckchavarria2@gmail.com>
2020-12-14 00:27:29 -0600

    Se agrega ventana de lista de productos y de detalle productos

---
+++ app/src/main/java/com/cuandoeramilitar/proyecto_final/Detalle_Producto.kt
@@ -0,0 +1,11 @@
+package com.cuandoeramilitar.proyecto_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Detalle_Producto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle__producto)
+    }
+}
